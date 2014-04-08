5a0aaa13f3cdbdd7c02a4453b10cd78c33b180ab
Matt Bertino <mattbertino@gmail.com>
2014-04-08 16:42:53 -0400

    Finished up the basic foundation of my site, needs polish

--- Gruntfile.js
+++ Gruntfile.js
@@ -313,7 +313,8 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
-                        'styles/fonts/{,*/}*.*'
+                        'styles/fonts/{,*/}*.*',
+                        'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*.*'
                     ]
                 }]
             },
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
 console.log('\'Allo \'Allo!');
+
+$('.thumbnail').hover(function() {
+  $(this).children('.thumbcontent').toggleClass('hidden')
+  $(this).css('cursor', 'pointer')
+})
