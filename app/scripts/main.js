87777844e4313db6933caf2e6b6e4ffcc27b91fa
Matt Bertino <mattbertino@gmail.com>
2014-04-10 16:25:52 -0400

    Fixed some things

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,10 +5,18 @@ $(".smooth").bind("click", function(e) {
   e.preventDefault();
   var location = $(this).attr("href")
 
-
-
   $('html, body').animate({
     scrollTop: $(location).offset().top - 68
   }, 1000);
 
 });
+
+$('.boom').click(function() {
+  $('.wrapper').toggleClass("hidden");
+  $('.personal-page').toggleClass("hidden");
+});
+
+$('.back').click(function() {
+  $('.wrapper').toggleClass("hidden");
+  $('.personal-page').toggleClass("hidden");
+})
